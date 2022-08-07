653136e8af5ac8373759ede4bb456758cfc286c4
alexandre <alex14fr@gmail.com>
2022-08-07 23:29:44 +0200

    x

--- src/dropcap-inner.c
+++ src/dropcap-inner.c
@@ -52,18 +52,19 @@ int main(int argc, char **argv) {
 
 	if(mount("none","/",NULL,MS_PRIVATE|MS_REC,NULL)<0) { perror("mount private /"); exit(1); }
 	chdir("/");
-	if(getenv("TERM_SHARE_FILES")!=NULL) {
-		unsigned char rnd[6];
+	unsigned char rnd[6];
+	if(getenv("TERM_SHARE_FILES")==NULL) 
 		getrandom(rnd, 6, 0);
-		snprintf(tmp, 256, "/tmp/priv%x%x%x%x%x%x", rnd[0], rnd[1], rnd[2], rnd[3], rnd[4], rnd[5]);
-		mkdir("/tmp", 0777);
-		if(mkdir(tmp, 0777)<0) { perror("mkdir"); }
-		if(mount(tmp,"/tmp",NULL,MS_PRIVATE|MS_BIND,NULL)<0) { perror("mount bind /tmp"); exit(1); }
-		mkdir("/tmp/pts",0777);
-		mkdir("/dev",0777);
-		mkdir("/dev/pts",0777);
-		if(mount("/tmp/pts","/dev/pts",NULL,MS_PRIVATE|MS_BIND,NULL)<0) { perror("mount bind /dev/pts"); exit(1); }
-	}
+	else
+		bzero(rnd, 6);
+	snprintf(tmp, 256, "/tmp/priv%x%x%x%x%x%x", rnd[0], rnd[1], rnd[2], rnd[3], rnd[4], rnd[5]);
+	mkdir("/tmp", 0777);
+	if(mkdir(tmp, 0777)<0) { perror("mkdir"); }
+	if(mount(tmp,"/tmp",NULL,MS_PRIVATE|MS_BIND,NULL)<0) { perror("mount bind /tmp"); exit(1); }
+	mkdir("/tmp/pts",0777);
+	mkdir("/dev",0777);
+	mkdir("/dev/pts",0777);
+	if(mount("/tmp/pts","/dev/pts",NULL,MS_PRIVATE|MS_BIND,NULL)<0) { perror("mount bind /dev/pts"); exit(1); }
 	prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0);
 	prctl(PR_SET_SECUREBITS, SECBIT_KEEP_CAPS_LOCKED|SECBIT_NO_SETUID_FIXUP|SECBIT_NO_SETUID_FIXUP_LOCKED|SECBIT_NOROOT|SECBIT_NOROOT_LOCKED|SECBIT_NO_CAP_AMBIENT_RAISE|SECBIT_NO_CAP_AMBIENT_RAISE_LOCKED, 0, 0, 0);
 	for(int i=0;i<1024;i++) {
